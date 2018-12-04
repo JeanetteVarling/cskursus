214b351aead7f1bcabdcfd123dcf9070bd22b2ec
jeanette@varling.dk <jeanette@varling.dk>
2018-12-04 15:17:45 +0100

    JEAV dag 4 1517

--- 24 1 Diagnosticering/Program.cs
+++ 24 1 Diagnosticering/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,8 @@ namespace _24_1_Diagnosticering
     {
         static void Main(string[] args)
         {
+
+            //
         }
     }
 }
